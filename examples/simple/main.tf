9380268aa259ad9b37676abbb837ac6f429356c7
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-06-16 11:33:03 +0100

    changed example deployment to use remote repo

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,6 +1,8 @@
 # Deploy the policy assignment module
-module "assign_policies" {
-  source   = "../../"
+
+module "management-group-policy-assignment" {
+  source  = "andrewCluey/management-group-policy-assignment/azurerm"
+  version = "0.1.0"
 
   archetype        = "platform"
   management_group = "im"
