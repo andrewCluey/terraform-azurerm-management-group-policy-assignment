572fdf316ec01fec7a8e96a6b7230267a5b1adfb
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-06-16 11:42:45 +0100

    added each.key to output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "id" {
-    value = azurerm_policy_assignment.assignment.id
+    value = azurerm_policy_assignment[each.key].assignment.id
     description = "The ID of the new policy assignment"
 }
