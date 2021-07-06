84c24442010c4c61ec18223f54918de82620759e
Andrew Clure <andrew@asc-solutions.co.uk>
2021-07-06 12:03:18 +0100

    re-formatted layout of code

--- README.md
+++ README.md
@@ -10,15 +10,14 @@ This
 # OVERVIEW
 A defintion file, known as an 'archetype' is used to simplify the application of policies to management groups of a similar 'type'.
 
-For example, an archetype file might be defined that specifies policies for a 'Sanbox' Management Group. This would require very stric policies for the type (& tier) of resources deployed along with stric limitations of how resources interact with other environments. In this case, an archetype woudl be defiend with policies declared to enfiorce these standards. 
+For example, an archetype file might be defined that specifies policies for a 'Sandbox' Management Group. This would require very stric policies for the type (& tier) of resources deployed along with strict limitations of how resources interact with other environments. In this case, an archetype would be defined with policies declared to enforce these standards. 
 
-The benefit of using these 'types', is that a management group can be assigned to a single group and receive all policies defined. This reduces the amount of duplication needed to assign policies. 
+The benefit of using these 'types', is that a management group can be assigned to a single archetype type and receive all policies defined. This reduces the amount of duplication needed to assign policies. 
 
-The drawback is that it does reduce flexibility. If a Management Group requires similar, but not the same, set of policies defined in an Archetytpe, then a new archetype will be required. 
+However, this may reduce flexibility. If a Management Group requires similar, but not the same, set of policies defined in an Archetytpe, then a new archetype will be required. 
 
-This idea was inspired by the Microsoft 'CAF' Terraform modules. Where a whole Enterprise Scale Azure infrastructre can be deployed using Terraform relatively simply. However, we foudn the underlying terraform code to achioeve this to be overly complex, which could introduce problems further down the line shoudl there be a problem. 
+Tghe use of Archetypes, and the method for defining them in code as part of the Terraform deployment, is define din the Microsoft 'CAF' Terraform modules. Where an Enterprise Scale Azure infrastructure can be deployed using Terraform. While the Microsoft CAF module achieves the deployment of a large, Enterprise Scale Azure environment, I found the code itself to be quite complex and in many cases, not required for smaller environments.
 
-So we have taken some of the best bits, and tailored it to our specific needs. 
 
 # DEEP DIVE
 
--- main.tf
+++ main.tf
@@ -1,38 +1,39 @@
 locals {
-    archetype_file          = "${var.archetype_files_path}/${var.archetype}.json" #"${path.module}/lib/types/${var.archetype}.json"
-    policy_assignment_files = var.assignment_file_path #"${path.module}/lib/policyAssignments"
-    management_group_scope  = "/providers/Microsoft.Management/managementGroups/${var.management_group}"
-    raw_archetype_data      = jsondecode(file(local.archetype_file))
-    policies_to_assign      = toset(local.raw_archetype_data.landing_zones.policy_assignments)
+  archetype_file          = "${var.archetype_files_path}/${var.archetype}.json"
+  policy_assignment_files = var.assignment_file_path
+  management_group_scope  = "/providers/Microsoft.Management/managementGroups/${var.management_group}"
+  raw_archetype_data      = jsondecode(file(local.archetype_file))
+  policies_to_assign      = toset(local.raw_archetype_data.landing_zones.policy_assignments)
 }
 
 
 
 resource "azurerm_policy_assignment" "assignment" {
-    for_each = local.policies_to_assign
+  for_each = local.policies_to_assign
 
-    # The policy assignment name length must not exceed '24' characters (FEATURE: Add in a validation step for this)
-    name                 = each.key
-    scope                = local.management_group_scope
-    policy_definition_id = jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.policyDefinitionId
-    display_name         = jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.displayName
-    location             = try(jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).location, null)
-    description          = try(jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.description, "${each.key} Policy Assignment at ${local.management_group_scope}")
-    metadata             = <<METADATA
+  # The policy assignment name length must not exceed '24' characters (FEATURE: Add in a validation step for this)
+  name                 = each.key
+  display_name         = jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.displayName
+  scope                = local.management_group_scope
+  policy_definition_id = jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.policyDefinitionId
+  location             = try(jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).location, null)
+  parameters           = try(jsonencode(jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.parameters), null)
+  not_scopes           = jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.notScopes
+  enforcement_mode     = jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.enforcementMode
+  metadata             = <<METADATA
     {
         "category": "General",
         "createdBy": "Terraform",
         "createdOn": "null"
     }
 METADATA
-    #metadata             = try(jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.metadata, null)
-    parameters           = try(jsonencode(jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.parameters), null)
-    not_scopes           = jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.notScopes
-    enforcement_mode     = jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.enforcementMode
-    
-    identity {
-        type = try(jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).identity.type, "None")
-    }
+
+  description = try(jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).properties.description,
+  "${each.key} Policy Assignment at ${local.management_group_scope}")
+
+  identity {
+    type = try(jsondecode(file("${local.policy_assignment_files}/${each.key}.json")).identity.type, "None")
+  }
 }
 
 
