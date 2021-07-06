4bc707fe8496e2ff4c69ef339012bac90e388061
Andrew Clure <andrew@asc-solutions.co.uk>
2021-07-06 12:04:05 +0100

    modified exmaple deployment layout

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -5,8 +5,8 @@ module "management_group_policy_assignment" {
   #source  = "andrewCluey/management-group-policy-assignment/azurerm"
   #version = "0.1.0"
 
-  archetype        = "platform"
-  management_group = "im"
+  archetype            = "platform"
+  management_group     = "im"
   assignment_file_path = "${path.module}/lib/policyAssignments"
   archetype_files_path = "${path.module}/lib/types"
 }
